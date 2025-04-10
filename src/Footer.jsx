0733fdd002b6fdb803a8ebf4643fff25918fadae
Steno-95 <lezzistefano95@gmail.com>
2025-04-10 15:54:44 +0200

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Stefano Lezzi</a>.
+    </div>
+  );
+}
+
+export default Footer;
